3cc1f6272f3317d62eca196701b07833c70bf53c
佳慧 康 <anny.kang@snowballtech.com>
2025-12-15 08:40:41 +0800

    test pass

--- lesson_2/common/functions.py
+++ lesson_2/common/functions.py
@@ -2,14 +2,6 @@
 import numpy as np
 
 
-def identity_function(x):
-    return x
-
-
-def step_function(x):
-    return np.array(x > 0, dtype=np.int)
-
-
 def sigmoid(x):
     return 1 / (1 + np.exp(-x))    
 
@@ -37,10 +29,6 @@ def softmax(x):
 
     x = x - np.max(x) # 溢出对策
     return np.exp(x) / np.sum(np.exp(x))
-
-
-def mean_squared_error(y, t):
-    return 0.5 * np.sum((y-t)**2)
 
 
 def cross_entropy_error(y, t):
