e0043f6727373854380aa902ed9bf24cc674b769
佳慧 康 <anny.kang@snowballtech.com>
2025-12-15 23:25:01 +0800

    优化训练器打印

--- lesson_2/train_neuralnet.py
+++ lesson_2/train_neuralnet.py
@@ -1,7 +1,6 @@
 # coding: utf-8
 import sys, os
 sys.path.append(os.pardir)
-
 import numpy as np
 from dataset.mnist import load_mnist
 from two_layer_net import TwoLayerNet
@@ -16,10 +15,6 @@ train_size = x_train.shape[0]
 batch_size = 100
 learning_rate = 0.1
 
-train_loss_list = []
-train_acc_list = []
-test_acc_list = []
-
 iter_per_epoch = max(train_size / batch_size, 1)
 
 for i in range(iters_num):
@@ -27,9 +22,6 @@ for i in range(iters_num):
     x_batch = x_train[batch_mask]
     t_batch = t_train[batch_mask]
     
-    # 梯度
-    # 使用数值微分计算梯度
-    #grad = network.numerical_gradient(x_batch, t_batch)
     # 使用误差反向传播法计算梯度
     grad = network.gradient(x_batch, t_batch)
     
@@ -38,14 +30,13 @@ for i in range(iters_num):
         network.params[key] -= learning_rate * grad[key]
     
     loss = network.loss(x_batch, t_batch)
-    train_loss_list.append(loss)
+    
     
     if i % iter_per_epoch == 0:
         train_acc = network.accuracy(x_train, t_train)
         test_acc = network.accuracy(x_test, t_test)
-        train_acc_list.append(train_acc)
-        test_acc_list.append(test_acc)
-        print(train_acc, test_acc)
+        print(f"Iteration {i+1}/{iters_num}, Loss: {loss:.4f}")
+        print(f"Train Accuracy: {train_acc:.4f}, Test Accuracy: {test_acc:.4f}")
 
 # 保存参数
 network.save_params("params.pkl")
